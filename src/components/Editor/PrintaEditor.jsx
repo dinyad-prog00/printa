50922066da2fef97139956159b8a0ee15ad86c6d
Dinyad <donatien.yeto@gmail.com>
2022-10-04 14:25:41 +0200

    fin extension

--- src/components/Editor/PrintaEditor.jsx
+++ src/components/Editor/PrintaEditor.jsx
@@ -1,17 +1,49 @@
-import {minimalSetup, EditorView} from "codemirror"
+import { minimalSetup, EditorView } from "codemirror"
 import { javascript } from "@codemirror/lang-javascript";
-const PrintEditor = ()=>{
+import { css } from "@codemirror/lang-css";
+import { python } from "@codemirror/lang-python";
+import { html } from "@codemirror/lang-html";
+
+import {PrintaLanguages} from "./languages"
+import { useState } from "react";
+
+
+const PrintEditor = () => {
+    const [text, setText] = useState(undefined);
+    const [lang,setLang] = useState(undefined);
+    
+
+    const handleFileNameChange = (name) => {
+        var i = name.indexOf(".");
+        const extension = name.substr(i + 1);
+
+        if (extension in PrintaLanguages) {
+            setText(PrintaLanguages[extension])
+        }
+        else {
+            setText(undefined)
+        }
+
+    }
 
     return (
         <div id="printaeditor">
-
+            Nom du fichier <input
+                type="text"
+                id="message"
+                name="message"
+                onChange={(e) => { handleFileNameChange(e.target.value) }}
+                
+            />
+            <h2> Langage : {text && text} {!text && "Langage non reconnu"}</h2>
         </div>
     );
 }
 
+
 new EditorView({
     
-    extensions: [ minimalSetup ,javascript()],
+    extensions: [minimalSetup, javascript()],
     parent: document.body
 })
 
---
+++ src/components/Editor/languages.js
@@ -0,0 +1,15 @@
+export  const PrintaLanguages = {
+    "js" : "JavaScript",
+    "py" : "Python",
+    "css" : "CSS",
+    "html" : "HTML",
+    "ts" : "TypeScript",
+    "xml" : "XML",
+    "java" : "Java",
+    "c" : "C",
+    "cpp" : "C++",
+    "dart" : "Dart",
+    "php" :"PHP",
+    
+
+}
